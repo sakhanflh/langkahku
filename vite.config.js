dc4d1b52df89ca4d4d2bd17e35b7f0a31d6d7c08
Sakha_Naufal_Huda <sakhanaufal2@gmail.com>
2025-09-08 12:58:56 +0700

    add manifest.json

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Langkahku',
+        short_name: 'Langkahku',
+        description: 'Deskripsi website kamu',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/img/langkahku-logo.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/img/langkahku-logo.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
 })
