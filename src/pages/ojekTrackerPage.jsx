72b4a3281899dd2d3a2160f5fb9b2519d5204a45
Sakha_Naufal_Huda <sakhanaufal2@gmail.com>
2025-09-08 11:57:46 +0700

    fix: add verceljson

--- src/pages/ojekTrackerPage.jsx
+++ src/pages/ojekTrackerPage.jsx
@@ -95,9 +95,8 @@ export default function OjekTrackerPage() {
     })).reverse();
 
     return (
-        <div className="min-h-screen bg-gray-800 text-gray-100 p-4 md:p-6">
+        <div className="min-h-screen bg-gray-800 text-gray-100 md:p-6">
             <HeaderSection />
-
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
                 {/* Form Input Aktivitas */}
                 <div className="lg:col-span-1">
